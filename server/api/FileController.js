b695601c21dbef1284b4c7125272a6379749a0d8
Cyril Pham-le <cyril.phamle@gmail.com>
2018-02-26 23:04:55 +0100

    upload profile picture

--- server/api/FileController.js
+++ server/api/FileController.js
@@ -21,19 +21,18 @@ module.exports = class FileController {
 
   updateFile() {
     const storage = multer.diskStorage({
-      destination: (req, file, callback) => {
-        callback(null, './public');
-      },
-      filename: (req, file, callback) => {
-        callback(null, file.originalname);
-      }
+      destination: (req, file, callback) => callback(null, 'public/'),
+      filename: (req, file, callback) => callback(null, file.originalname)
     });
-    this.upload = multer({ storage : storage }).array('uploads[]', 12);
+    this.upload = multer({ storage : storage }).single('picture');
 
     this.app.post('/upload', (req, res) => {
       console.log(`${new Date()} -- [FileController] Upload file - Headers: ${JSON.stringify(req.headers)}`);
       this.upload(req, res, (err) => {
-        if (err) res.send({ code: 500, response: 'File not accepted' });
+        if (err) {
+          console.log(`${new Date()} -- [FileController] Upload file - Error: ${err}`);
+          res.send({ code: 500, response: 'File not accepted' });
+        }
         res.send({ code: 200 });
       });
     });
